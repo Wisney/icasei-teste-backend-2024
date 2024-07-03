4b835c5e8f2724fd152a291e5f4872f985b8029b
Wisney <wisneymaciel2@hotmail.com>
2024-07-03 17:57:25 -0300

    FIX: ProductConsumer

--- ms-rails/app/consumers/product_consumer.rb
+++ ms-rails/app/consumers/product_consumer.rb
@@ -2,21 +2,10 @@ class ProductConsumer < Karafka::BaseConsumer
   def consume(messages)
     messages.each do |message|
       product = message.payload
-      ::Services::Api::V1::Products::Upsert.new(product_params.merge({is_api: false}), product).execute
+      product['is_api'] = false
+      product.symbolize_keys!
+      
+      ::Services::Api::V1::Products::Upsert.new(product, nil).execute
     end
   end
 end
-
-private
-  def product_params
-    params&.permit(
-      [
-        :id,
-        :name,
-        :brand,
-        :price,
-        :description,
-        :stock
-      ]
-    )
-  end
